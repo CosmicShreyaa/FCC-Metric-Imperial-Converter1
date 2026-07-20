6b2fe090a818b045a7a60ea289f6821420de816f
ShreyaaBrillix <docweirdoshreya2@gmail.com>
2026-07-20 19:06:28 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const apiRoutes = require('./routes/api.js');
+const fccTestingRoutes = require('./routes/fcctesting.js');
+const runner = require('./test-runner.js');
+
+const app = express();
+
+app.use(cors({ origin: '*' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+// For FCC testing purposes
+fccTestingRoutes(app);
+
+apiRoutes(app);
+
+app.use((req, res) => {
+  res.status(404).type('text').send('Not Found');
+});
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port);
+
+  if (process.env.NODE_ENV === 'test') {
+    console.log('Running Tests...');
+    setTimeout(() => {
+      try {
+        runner.run();
+      } catch (e) {
+        console.log('Tests are not valid:');
+        console.error(e);
+      }
+    }, 1500);
+  }
+});
+
+module.exports = app;
